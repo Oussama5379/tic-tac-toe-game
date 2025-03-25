d017d2b4a6fbf2bfc7c21d585beba9f9fd440057
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-03-25 11:48:15 +0100

    Make the game functional in the console

--- script.js
+++ script.js
@@ -12,11 +12,117 @@ function GameBoard() {
 
 	const getBoard = () => board;
 
-	const markCell = (row, column, player) => {
-		if (board[row][column] === 1 || board[row][column] === 2) {
+	const printBoard = () => {
+		const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()));
+		console.log(boardWithCellValues);
+	};
+
+	const markCell = (row, column, number) => {
+		board[row][column].addMark(number);
+	};
+	return { getBoard, markCell, printBoard };
+}
+function Cell() {
+	let value = 0;
+	const addMark = (number) => {
+		value = number;
+	};
+	const getValue = () => value;
+
+	return { addMark, getValue };
+}
+
+function GameController(firstPlayerName = "playerOne", secondPlayerName = "secondPlayer") {
+	const gameBoard = GameBoard();
+	const players = [
+		{
+			name: firstPlayerName,
+			number: 1,
+		},
+		{
+			name: secondPlayerName,
+			number: 2,
+		},
+	];
+	let currentPlayer = players[0];
+	const getCurrentPlayer = () => currentPlayer;
+	const switchTurn = () => {
+		if (currentPlayer === players[0]) {
+			currentPlayer = players[1];
+		} else {
+			currentPlayer = players[0];
+		}
+	};
+	const startNewRound = () => {
+		gameBoard.printBoard();
+		console.log(currentPlayer.name + "'s turn");
+	};
+	const playRound = (player) => {
+		let colChoice = prompt("give the column");
+		let rowChoice = prompt("give the row");
+		while (
+			gameBoard.getBoard()[rowChoice][colChoice].getValue() === 1 ||
+			gameBoard.getBoard()[rowChoice][colChoice].getValue() === 2
+		) {
+			colChoice = prompt("give the column");
+			rowChoice = prompt("give the row");
+		}
+
+		gameBoard.markCell(rowChoice, colChoice, player.number);
+		if (playerWin(player, gameBoard.getBoard())) {
+			console.log(player.name + " wins");
+			return;
+		} else if (fullBoard(gameBoard.getBoard())) {
+			console.log("NO body wins");
 			return;
 		}
-		board[row][column] = player.number;
+		switchTurn();
+		startNewRound();
 	};
-	return { getBoard, markCell };
+	function playerWin(player, board) {
+		for (let i = 0; i < 3; i++) {
+			if (
+				board[i][0].getValue() === player.number &&
+				board[i][1].getValue() === player.number &&
+				board[i][2].getValue() === player.number
+			) {
+				return true;
+			}
+			if (
+				board[0][i].getValue() === player.number &&
+				board[1][i].getValue() === player.number &&
+				board[2][i].getValue() === player.number
+			) {
+				return true;
+			}
+		}
+		if (
+			board[0][0].getValue() === player.number &&
+			board[1][1].getValue() === player.number &&
+			board[2][2].getValue() === player.number
+		) {
+			return true;
+		}
+		if (
+			board[0][2].getValue() === player.number &&
+			board[1][1].getValue() === player.number &&
+			board[2][0].getValue() === player.number
+		) {
+			return true;
+		}
+		return false;
+	}
+	function fullBoard(board) {
+		for (let i = 0; i < 3; i++) {
+			for (let j = 0; j < 3; j++) {
+				if (board[i][j].getValue() !== 1 && board[i][j].getValue() !== 2) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	startNewRound();
+	return { playRound, getCurrentPlayer };
 }
+const game = GameController();
